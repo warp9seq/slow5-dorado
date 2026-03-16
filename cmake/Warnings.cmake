18e776a48dd28f70186a0911427ee87859ce3044
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2026-03-16 18:32:50 +1100

    warning diable

--- cmake/Warnings.cmake
+++ cmake/Warnings.cmake
@@ -19,8 +19,9 @@ function(enable_warnings_as_errors TARGET_NAME)
         )
     elseif(CMAKE_COMPILER_IS_GNUCXX)
         target_compile_options(${TARGET_NAME} PRIVATE
-            -Wall -Wextra -Werror -Wundef -Wshadow
+            -Wall -Wextra -Wundef -Wshadow
             -Wmissing-declarations -Wno-error=maybe-uninitialized
+            #todo -Werror is failing for rocm torch
         )
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
         target_compile_options(${TARGET_NAME} PRIVATE
